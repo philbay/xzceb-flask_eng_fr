b3ce0221966fb501384b8360de08a618fc24084c
philbay <philemon.stjean@esih.edu>
2023-01-19 00:42:18 +0100

    adding correction for api

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,12 +9,11 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
-version = '2018-05-01'
 
 """ create an instance of the IBM Watson Language translator"""
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version='{version}',
+    version='2018-05-01',
     authenticator=authenticator
 )
 
@@ -22,10 +21,12 @@ language_translator.set_service_url(url)
 #language_translator.set_disable_ssl_verification(True) # disabling ssl verification
 
 def englishToFrench(englishText):
+    """ This method translates from english to french """
     frenchText=''
     try:
     # Invoke a method
-        translation = language_translator.translate( text=englishText, 
+        translation = language_translator.translate(
+        text=englishText, 
         model_id='en-fr').get_result()
         frenchText = json.dumps(translation, indent=2, ensure_ascii=False)
     except ApiException as ex:
@@ -35,8 +36,19 @@ def englishToFrench(englishText):
     return frenchText
 
 def frenchToEnglish(frenchText):
-    #write the code here
-    englishText = ""
+    """ This method translates from french to english """
+    englishText=''
+    try:
+    # Invoke a method
+        translation = language_translator.translate(
+        text=frenchText, 
+        model_id='fr-en').get_result()
+        englishText = json.dumps(translation, indent=2, ensure_ascii=False)
+    except ApiException as ex:
+        print 
+        "Method failed with status code " + str(ex.code) + ": " + ex.message
+    
     return englishText
 
-print(englishToFrench(""))
+print(englishToFrench("Good morning sir"))
+print(frenchToEnglish("Fatigue, nervosité, fatigue musculaire sont souvent les premiers signes d'un apport alimentaire insuffisant en magnésium"))
