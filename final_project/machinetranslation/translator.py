ee7c4bdd50a8b69465f1b9609dd8b25b47f20308
Philemon St Jean <FU924PSJ@fime.com>
2023-01-18 23:42:39 +0100

    start method englishToFrench

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -3,11 +3,13 @@ from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import os
 from dotenv import load_dotenv
+from ibm_watson import ApiException
 
 load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+version = '2018-05-01'
 
 """ create an instance of the IBM Watson Language translator"""
 authenticator = IAMAuthenticator(apikey)
@@ -17,9 +19,24 @@ language_translator = LanguageTranslatorV3(
 )
 
 language_translator.set_service_url(url)
+#language_translator.set_disable_ssl_verification(True) # disabling ssl verification
 
 def englishToFrench(englishText):
-    frenchText = "Hello, world"
-    translation = language_translator.translate( text=frenchText, model_id='en-fr').get_result()
-    print(json.dumps(translation, indent=2, ensure_ascii=False))
+    frenchText=''
+    try:
+    # Invoke a method
+        translation = language_translator.translate( text=englishText, 
+        model_id='en-fr').get_result()
+        frenchText = json.dumps(translation, indent=2, ensure_ascii=False)
+    except ApiException as ex:
+        print 
+        "Method failed with status code " + str(ex.code) + ": " + ex.message
+   
     return frenchText
+
+def frenchToEnglish(frenchText):
+    #write the code here
+    englishText = ""
+    return englishText
+
+print(englishToFrench(""))
